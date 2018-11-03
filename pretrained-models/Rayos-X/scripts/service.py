89bd537f7324d973b705f0c8031b7f45c666e71b
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2018-11-03 18:03:42 -0500

    Updating score

--- pretrained-models/Rayos-X/scripts/service.py
+++ pretrained-models/Rayos-X/scripts/service.py
@@ -23,15 +23,19 @@ def rayosxy():
 	# dimensions of our images.
 	img_width, img_height = 299, 299
 	# Show
-	img_path='../samples/img1.png' #change to location of chest x-ray
+	img_path='../samples/img1.png'
 	img = image.load_img(img_path, target_size=(img_width, img_height))
 	img = image.img_to_array(img)
 	x = np.expand_dims(img, axis=0) * 1./255
 
 	with graph.as_default():
 		score = loaded_model.predict(x)
-		#print('Prediccion:', score, 'Abdomen X-ray' if score < 0.5 else 'Pulmon X-ray')
-		return 'Prediccion:' + score + 'Abdomen X-ray' if score < 0.5 else 'Pulmon X-ray'
+		if score < 0.5
+			resultado = 'Prediccion: Abdomen X-ray , score: ' + score
+		else
+		    resultado = 'Prediccion: Pulmon X-ray , score: ' + score
+		print('Prediccion:', score, 'Abdomen X-ray' if score < 0.5 else 'Pulmon X-ray')
+		return resultado
 
 # Run de application
 app.run(host='0.0.0.0',port=5000)

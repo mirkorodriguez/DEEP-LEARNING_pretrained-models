0890f5a5eb372973de6916e13940e44897e7a8ee
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2018-11-03 17:36:51 -0500

    Updating calls

--- pretrained-models/Rayos-X/scripts/cnn_executor.py
+++ pretrained-models/Rayos-X/scripts/cnn_executor.py
@@ -6,41 +6,16 @@
 # -----------------------------------------------------------
 # Cargando modelo de disco
 from keras.models import model_from_json
-from keras.preprocessing import image
-import numpy as np
 import matplotlib.pyplot as plt
 from keras.optimizers import Adam
 
-# dimensions of our images.
-img_width, img_height = 299, 299
-
-json_file = open('../model/rx_model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("../model/rx_model.h5")
-print("Cargando modelo desde el disco ...")
-loaded_model.compile(optimizer=Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08,decay=0.0), loss='binary_crossentropy', metrics=['accuracy'])
-print("Modelo cargado de disco!")
-
-# Show
-img_path='../samples/img1.png' #change to location of chest x-ray
-img = image.load_img(img_path, target_size=(img_width, img_height))
-# Predict
-plt.imshow(img)
-plt.show()
-img = image.img_to_array(img)
-x = np.expand_dims(img, axis=0) * 1./255
-score = loaded_model.predict(x)
-print('Prediccion:', score, 'Abdomen X-ray' if score < 0.5 else 'Pulmon X-ray')
-
-
-img_path='../samples/img2.png' #change to location of chest x-ray
-img = image.load_img(img_path, target_size=(img_width, img_height))
-# Predict
-plt.imshow(img)
-plt.show()
-img = image.img_to_array(img)
-x = np.expand_dims(img, axis=0) * 1./255
-score = loaded_model.predict(x)
-print('Prediccion:', score, 'Abdomen X-ray' if score < 0.5 else 'Pulmon X-ray')
+def cargarModelo():
+    json_file = open('../model/rx_model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights("../model/rx_model.h5")
+    print("Cargando modelo desde el disco ...")
+    loaded_model.compile(optimizer=Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08,decay=0.0), loss='binary_crossentropy', metrics=['accuracy'])
+    print("Modelo cargado de disco!")
+    return loaded_model
--- pretrained-models/Rayos-X/scripts/service.py
+++ pretrained-models/Rayos-X/scripts/service.py
@@ -1,13 +1,42 @@
 #Import Flask
 from flask import Flask
+from keras.preprocessing import image
+import numpy as np
 
 #Initialize the application service
 app = Flask(__name__)
+global model_loaded
+model_loaded = cargarModelo()
 
 #Define a route
-@app.route('/rayos-x/')
-def hello_world():
-	return 'Hello World!'
+@app.route('/')
+def main_page():
+	return 'Bienvenido a la URP - RNA!'
+
+@app.route('/rayos-x/', methods=['GET','POST'])
+def rayosx():
+	return 'Modelo Rayos-X!'
+
+@app.route('/rayos-x/default/', methods=['GET','POST'])
+def rayosxy():
+	# dimensions of our images.
+	img_width, img_height = 299, 299
+	# Show
+	img_path='../samples/img1.png' #change to location of chest x-ray
+	img = image.load_img(img_path, target_size=(img_width, img_height))
+	img = image.img_to_array(img)
+	x = np.expand_dims(img, axis=0) * 1./255
+	score = loaded_model.predict(x)
+	print('Prediccion:', score, 'Abdomen X-ray' if score < 0.5 else 'Pulmon X-ray')
+
+	img_path='../samples/img2.png' #change to location of chest x-ray
+	img = image.load_img(img_path, target_size=(img_width, img_height))
+	img = image.img_to_array(img)
+	x = np.expand_dims(img, axis=0) * 1./255
+	score = loaded_model.predict(x)
+	#print('Prediccion:', score, 'Abdomen X-ray' if score < 0.5 else 'Pulmon X-ray')
+
+	return 'Prediccion:' + score + 'Abdomen X-ray' if score < 0.5 else 'Pulmon X-ray'
 
 # Run de application
 app.run(host='0.0.0.0',port=5000)

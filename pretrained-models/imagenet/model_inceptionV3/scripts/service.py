e74b1356d2a219e5bb68a600e2ad2fe0c2e4bac7
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2018-11-22 22:45:17 -0500

    Adding uploading functions

--- pretrained-models/imagenet/model_inceptionV3/scripts/service.py
+++ pretrained-models/imagenet/model_inceptionV3/scripts/service.py
@@ -68,22 +68,25 @@ def default():
 
 	return jsonify(data)
 
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/inceptionv3/predict/',methods=['POST'])
 def predict():
 
 	data = {"success": False}
 
 	if request.method == "POST":
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            print('No file part')
-        file = request.files['file']
-        # if user does not select file, browser also submit a empty part without filename
-        if file.filename == '':
-            print('No selected file')
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			print('No file part')
+		file = request.files['file']
+		# if user does not select file, browser also submit a empty part without filename
+		if file.filename == '':
+			print('No selected file')
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
 			#loading image
 			filename = UPLOAD_FOLDER + 'name'
